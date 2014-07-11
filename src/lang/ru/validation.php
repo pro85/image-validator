1824b94e1dff2770a5791319ce6fdc476e5c7bb5
Pro <pro@realcomp.ru>
2014-07-11 16:05:08 +0400

    Add russian validation messages

---
+++ src/lang/ru/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'image_size'          => ':attribute должно быть шириной :width и высотой :height.',
+
+	'between'            => 'между :size1 и :size2 пикселей',
+	'lessthan'           => 'меньше чем :size пикселей',
+	'lessthanorequal'    => 'меньше или равным :size пикселям',
+	'greaterthan'        => 'больше чем :size пикселей',
+	'greaterthanorequal' => 'больше или равным :size пикселям',
+	'equal'              => ':size пикселей',
+	'anysize'            => 'любой размер',
+
+	'image_aspect'       => ':attribute соотношенис сторон должно быть :aspect.',
+);
